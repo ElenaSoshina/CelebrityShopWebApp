282fb3b44cb793504ac2b8016734e0f94598321b
ElenaSoshina <soshinaalena6415@gmail.com>
2025-04-09 19:49:49 +0300

    change homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx - Main application component
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { Header } from './components/Header/Header';
 import Hero from './components/Hero/Hero';
@@ -9,7 +9,6 @@ import { GiftCards } from './components/GiftCards/GiftCards';
 import { PCGames } from './components/PCGames/PCGames';
 import { GameStore } from './components/GameStore/GameStore';
 import { ShooterGames } from './components/ShooterGames/ShooterGames';
-import Home from './pages/Home/Home';
 import { BloodStrike } from './pages/BloodStrike/BloodStrike';
 
 function App() {
@@ -19,8 +18,18 @@ function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={
+              <>
+                <Hero />
+                <GameStore />
+                <SuperSellGames />
+                <GiftCards />
+                <PCGames />
+                <ShooterGames />
+              </>
+            } />
             <Route path="/game/blood-strike" element={<BloodStrike />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
       </div>
