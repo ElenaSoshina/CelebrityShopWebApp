2aaeaceffe65568e2b48b3f8cb8c0182294f8d52
ElenaSoshina <soshinaalena6415@gmail.com>
2025-04-16 21:23:41 +0300

    add ContactForm

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -95,7 +95,7 @@ export const GamePage: React.FC = () => {
         const response = await axios.get<ApiResponse>(`https://celebrity-strike.duckdns.org/api/v1/games/brawl_stars`, {
           headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/json'
           }
         });
         if (response.data.success) {
